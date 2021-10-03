7db7696d5b4373c304314031456a30869bcdbc13
andreysoft <56181301+andreysoft@users.noreply.github.com>
2021-10-03 03:29:55 -0400

    RecyclerView with text items

--- app/src/main/java/com/github/andreysoft/affirmations/MainActivity.kt
+++ app/src/main/java/com/github/andreysoft/affirmations/MainActivity.kt
@@ -3,7 +3,10 @@ package com.github.andreysoft.affirmations
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.github.andreysoft.affirmations.adapter.ItemAdapter
 import com.github.andreysoft.affirmations.data.Datasource
+import com.github.andreysoft.affirmations.model.Affirmation
 
 class MainActivity : AppCompatActivity()
 {
@@ -12,7 +15,10 @@ class MainActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView : TextView= findViewById(R.id.textview)
-        textView.text = Datasource().loadAffirmations().size.toString()
+        val myDataset = Datasource().loadAffirmations()
+        val recycleView = findViewById<RecyclerView>(R.id.recycler_view)
+        recycleView.setHasFixedSize(true)
+        recycleView.adapter = ItemAdapter(this, myDataset)
+
     }
 }
---
+++ app/src/main/java/com/github/andreysoft/affirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,40 @@
+package com.github.andreysoft.affirmations.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.DateSorter
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.github.andreysoft.affirmations.R
+import com.github.andreysoft.affirmations.model.Affirmation
+
+class ItemAdapter(
+    private val context: Context,
+    private val dataset: List<Affirmation>
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>()
+{
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder
+    {
+        val adapterLayout  = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int)
+    {
+        val item : Affirmation = dataset[position]
+        holder.textview.text = context.resources.getString(item.stringResourceId)
+    }
+
+    override fun getItemCount() =  dataset.size
+
+    ///
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view)
+    {
+        val textview: TextView = view.findViewById(R.id.item_title)
+
+    }
+
+}
